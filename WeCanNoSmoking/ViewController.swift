c1ad4847e05341b234192e95e4a2e22ea52f8739
kangddong <rkdehddud96@naver.com>
2023-09-19 00:46:26 +0900

    .DS_Store is removed

---
+++ WeCanNoSmoking/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WeCanNoSmoking
+//
+//  Created by 강동영 on 2023/09/19.
+//
+
+import UIKit
+
+final class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
